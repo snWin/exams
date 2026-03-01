bc15c845c3107e1e731241bcdb4ebd7acbec5a80
Su Ni Win <suniwinn@gmail.com>
2026-03-02 02:18:48 +0800

    Commit for SimpleFileSystem

---
+++ SimpleFileSystem/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleFileSystem
+{
+	class SimpleFileSystem
+	{
+		private string rootDirectory;
+
+		public SimpleFileSystem(string rootPath)
+		{
+			rootDirectory = rootPath;
+
+			if (!Directory.Exists(rootDirectory))
+			{
+				Directory.CreateDirectory(rootDirectory);
+			}
+		}
+
+		private string GetFullPath(string fileName)
+		{
+			return Path.Combine(rootDirectory, fileName);
+		}
+
+		public void CreateFile(string fileName)
+		{
+			string path = GetFullPath(fileName);
+
+			if (File.Exists(path))
+			{
+				Console.WriteLine("File already exists.");
+				return;
+			}
+
+			File.Create(path).Close();
+			Console.WriteLine("File created successfully.");
+		}
+
+		public void WriteFile(string fileName, string content)
+		{
+			string path = GetFullPath(fileName);
+
+			if (!File.Exists(path))
+			{
+				Console.WriteLine("File does not exist.");
+				return;
+			}
+
+			File.WriteAllText(path, content);
+			Console.WriteLine("Content written to file.");
+		}
+
+		public void ReadFile(string fileName)
+		{
+			string path = GetFullPath(fileName);
+
+			if (!File.Exists(path))
+			{
+				Console.WriteLine("File does not exist.");
+				return;
+			}
+
+			string content = File.ReadAllText(path);
+			Console.WriteLine("File Content:");
+			Console.WriteLine(content);
+		}
+
+		public void DeleteFile(string fileName)
+		{
+			string path = GetFullPath(fileName);
+
+			if (!File.Exists(path))
+			{
+				Console.WriteLine("File does not exist.");
+				return;
+			}
+
+			File.Delete(path);
+			Console.WriteLine("File deleted.");
+		}
+
+		public void ListFiles()
+		{
+			string[] files = Directory.GetFiles(rootDirectory);
+
+			if (files.Length == 0)
+			{
+				Console.WriteLine("No files found.");
+				return;
+			}
+
+			Console.WriteLine("Files:");
+			foreach (string file in files)
+			{
+				Console.WriteLine(Path.GetFileName(file));
+			}
+		}
+	}
+
+	internal class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//}
+		static void Main()
+		{
+			//string root = Path.Combine(Environment.CurrentDirectory, "MyFileSystem"); // will create in bin\Debug
+			string root = Path.Combine(@"D:\exam\exams", "MyFileCollection");
+
+			SimpleFileSystem fs = new SimpleFileSystem(root);
+
+			while (true)
+			{
+				Console.WriteLine("\n=== Simple File System ===");
+				Console.WriteLine("1. Create File");
+				Console.WriteLine("2. Write File");
+				Console.WriteLine("3. Read File");
+				Console.WriteLine("4. Delete File");
+				Console.WriteLine("5. List Files");
+				Console.WriteLine("6. Exit");
+				Console.Write("Choose option: ");
+
+				string choice = Console.ReadLine();
+
+				switch (choice)
+				{
+					case "1":
+						Console.Write("File name: ");
+						fs.CreateFile(Console.ReadLine() + ".txt");
+						break;
+
+					case "2":
+						Console.Write("File name: ");
+						string writeName = Console.ReadLine();
+						Console.Write("Content: ");
+						string content = Console.ReadLine();
+						fs.WriteFile(writeName, content);
+						break;
+
+					case "3":
+						Console.Write("File name: ");
+						fs.ReadFile(Console.ReadLine());
+						break;
+
+					case "4":
+						Console.Write("File name: ");
+						fs.DeleteFile(Console.ReadLine());
+						break;
+
+					case "5":
+						fs.ListFiles();
+						break;
+
+					case "6":
+						return;
+
+					default:
+						Console.WriteLine("Invalid option.");
+						break;
+				}
+			}
+		}
+	}
+}
---
+++ XmlChecker/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlChecker
+{
+	class Program
+	{
+
+		//Count is correct
+	public static void Main()
+		{
+			string xmlContent = @"
+			<RandomTag123 RandomAttribute1=""2.0"" RandomAttribute2=""2022-10-07T09:12:41Z"">
+			  <RandomTag456 RandomAttribute3=""27012023-005"" RandomAttribute4=""100"" RandomAttribute5=""999"">
+				<RandomTag789 RandomAttribute6=""Normal"" RandomAttribute7=""1"" />
+				<RandomTag101112 RandomAttribute8="""" RandomAttribute9=""1101-01-001-A0001A01-R"" RandomAttribute10=""0"" RandomAttribute11=""1"" RandomAttribute12=""2022-11-11"" />
+			  </RandomTag456>
+			  <RandomTag456 RandomAttribute3=""27012023-006"" RandomAttribute4=""100"" RandomAttribute5=""999"">
+				<RandomTag789 RandomAttribute6=""Normal"" RandomAttribute7=""1"" />
+				<RandomTag101112 RandomAttribute8="""" RandomAttribute9=""1101-01-001-A0001A01-R"" RandomAttribute10=""0"" RandomAttribute11=""1"" RandomAttribute12=""2022-11-11"" />
+			  </RandomTag456>
+			</RandomTag123>";
+
+			//<Messages>
+			//    <Message>Content 1</Message>
+			//    <Message>Content 2</Message>
+			//    <Message>Content 3</Message>
+			//</Messages>";
+
+			
+			try
+			{
+				// Returns the parsed XDocument
+				XDocument doc = XDocument.Parse(xmlContent);
+
+				// Count all child elements named 'Message'
+				//int messageCount = doc.Descendants("Messages").Count();
+				int messageCount = doc.Descendants("RandomTag456").Count();
+
+				Console.WriteLine("Parsed successfully. It is valid xml.");
+				Console.WriteLine($"Number of messages: {messageCount}");
+
+			}
+			catch (XmlException ex)
+			{
+				// Throws exception if parsing fails
+				Console.WriteLine($"Invalid XML: {ex.Message}");
+			}
+			
+		}
+	}
+}
